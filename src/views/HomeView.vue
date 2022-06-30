ff1ac5250afa2c527a0e870237ce3b8685529c5e
AlfredTakoi <alfredtakoi@gmail.com>
2022-06-30 10:42:06 +0700

    initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,48 @@
+<template lang="">
+    <div>
+        <div>     
+            <div class="form-group">
+                <label for="">Username</label>
+                <input type="text"
+                class="form-control" name="" id="" v-model="username" aria-describedby="helpId" placeholder="">
+            </div>
+            <div class="form-group">
+                <label for="">Password</label>
+                <input type="text"
+                class="form-control" name="" id="" v-model="password" aria-describedby="helpId" placeholder="">
+            </div>
+            <div>
+                <button type="submit" @click="submitLogin">Login</button>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import axios from 'axios'
+
+export default {
+    name: 'LoginView',
+    data(){
+        return {
+            username: '',
+            password: ''
+        }
+    },
+    methods: {
+        submiLogin(){
+            axios.post('http://94.74.86.174:8080/api/',{
+                username: this.username,
+                password: this.password
+            })
+            .then((res)=>{
+                console.log(res)
+            }).catch((err)=>{
+                console.log(err)
+            })
+        }
+    }
+}
+</script>
+<style lang="">
+    
+</style>
